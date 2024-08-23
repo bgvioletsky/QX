59be8bb6b97a3631077294feb031b7f47123c20d
bgcode <violetcode@qq.com>
2024-08-23 18:31:09 +0800

    更新

--- rewrite_remote/xbs/render.js
+++ rewrite_remote/xbs/render.js
@@ -2,11 +2,11 @@
  * @Author: bgcode
  * @Date: 2024-08-05 12:51:41
  * @LastEditors: bgcode
- * @LastEditTime: 2024-08-09 13:15:45
+ * @LastEditTime: 2024-08-23 18:29:57
  * @Description: 
  * @FilePath: /QX/rewrite_remote/xbs/render.js
  */
-const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/QX@0.0.7/rewrite_remote/xbs/render.html";
+const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/QX@0.1.5/rewrite_remote/xbs/render.html";
 const myRequest = {
     url: url
 };
